3a78604808ab223fe7c4910604b6d032fa148ef0
user <danielsstenlijs@gmail.com>
2018-10-01 14:00:43 +0300

    20181001_14_00

---
+++ dgr_20181001.py
@@ -0,0 +1,53 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> vars()
+{'__builtins__': <module '__builtin__' (built-in)>, '__name__': '__main__', '__doc__': None, '__package__': None}
+>>> __builtins__.__doc__
+"Built-in functions, exceptions, and other objects.\n\nNoteworthy: None is the `nil' object; Ellipsis represents `...' in slices."
+>>> print(__builtins__.__doc__)
+Built-in functions, exceptions, and other objects.
+
+Noteworthy: None is the `nil' object; Ellipsis represents `...' in slices.
+>>> print(__builtins__.__doc__)
+Built-in functions, exceptions, and other objects.
+
+Noteworthy: None is the `nil' object; Ellipsis represents `...' in slices.
+>>> print(__builtins__.__doc__)
+Built-in functions, exceptions, and other objects.
+
+Noteworthy: None is the `nil' object; Ellipsis represents `...' in slices.
+>>> print(__builtins__.__doc__)
+Built-in functions, exceptions, and other objects.
+
+Noteworthy: None is the `nil' object; Ellipsis represents `...' in slices.
+>>> print(__builtins__.__doc__)
+Built-in functions, exceptions, and other objects.
+
+Noteworthy: None is the `nil' object; Ellipsis represents `...' in slices.
+>>> print(__builtins__.__doc__)
+Built-in functions, exceptions, and other objects.
+
+Noteworthy: None is the `nil' object; Ellipsis represents `...' in slices.
+>>> a=10
+>>> b=1.56
+>>> c='W'
+>>> vars()
+{'a': 10, 'c': 'W', 'b': 1.56, '__builtins__': <module '__builtin__' (built-in)>, '__package__': None, '__name__': '__main__', '__doc__': None}
+>>> a*a
+100
+>>> b*b
+2.4336
+>>> c*c
+
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    c*c
+TypeError: can't multiply sequence by non-int of type 'str'
+>>> type(a)
+<type 'int'>
+>>> type(b)
+<type 'float'>
+>>> type(c)
+<type 'str'>
+>>> 
---
+++ test_1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+a = 10
+b = 1.56
+c = 'W'
+# http://www.cplusplus.com/reference/cstdio/printf/
+print("a = %d"%(a))
+print("b = %.4f"%(b))
+print("c = %s"%(c))
+
+a = 20
+b = 3.49
+c = 'Q'
+print("a = %d"%(a))
+print("b = %.4f"%(b))
+print("c = %s"%(c))

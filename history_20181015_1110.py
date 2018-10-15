a09f68eeb39be32f2c5a368f77064988dff63ed9
user <danielsstenlijs@gmail.com>
2018-10-15 11:47:24 +0300

    20181015_11_45

---
+++ history_20181015_1110.py
@@ -0,0 +1,15 @@
+    1  VirtualBox --startvm XP
+    2  quartus
+    3  firefox &
+    4  git clone https://github.com/stenlijs/RTR105 
+    5  rm -rf RTR105 
+    6  git clone https://github.com/stenlijs/RTR105 
+    7  pwd
+    8  ls -l
+    9  cd RTR105
+   10  idle &
+   11  ls -l
+   12  -ls -l
+   13  pwd
+   14  ls -l
+   15  idle
---
+++ history_20181015_1133.py
@@ -0,0 +1,23 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> x = 5
+>>> if x < 10
+SyntaxError: invalid syntax
+>>> if x < 10:
+	print('smaller')
+	if x > 20:
+		print('bigger')
+
+		
+smaller
+>>> print('finis')
+finis
+>>> x=5
+>>> print('before 5')
+before 5
+>>> if x ==5
+SyntaxError: invalid syntax
+>>> if x==5
+SyntaxError: invalid syntax
+>>> if x ==5

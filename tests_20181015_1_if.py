e5168c65d4f3ebec6c191012bafe9814be1ca34b
user <danielsstenlijs@gmail.com>
2018-10-15 13:52:39 +0300

    tests_20181015_1_if.py

---
+++ tests_20181015_1_if.py
@@ -0,0 +1,55 @@
+x=34
+if x > 1:
+    print('1. skaitlis ir lielāks par 1')
+    if x < 100 :
+        print('1. skaitlis mazāks par 100')
+print('1. all done')
+
+x=4
+if x > 2:
+    print('2. lielāks')
+else:
+    print('2. nav lielāks')
+print('2. all done')
+
+x = 0
+if x < 2:
+    print('3. small')
+elif x < 10:
+    print('3. medium')
+else :
+    print('3. large')
+print('3. all done')
+
+x = 5
+if x < 2:
+    print('4. small')
+elif x < 10:
+    print('4. medium')
+else :
+    print('4. large')
+print('4. all done')
+
+
+x = 34
+if x < 2:
+    print('5. small')
+elif x < 10:
+    print('5. medium')
+else :
+    print('5. large')
+print('5. all done')
+
+x=34
+if x < 2:
+    print('6.small')
+elif x < 10:
+    print('6.medium')
+elif x < 20:
+    print('6.big')
+elif x < 40:
+    print('6.large')
+elif x < 100:
+    print('6.huge')
+else :
+    print('6.ginormous')

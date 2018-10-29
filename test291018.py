b2cf2ae2ae49049ccceeac888748072ce87adc3c
user <danielsstenlijs@gmail.com>
2018-10-29 11:22:04 +0200

    20182910_11_22

---
+++ test291018.py
@@ -0,0 +1,72 @@
+def thing():
+    print('hello')
+    print('fun')
+
+thing()
+print('zip')
+thing()
+
+print('-----------------')
+
+big=max('hello world')
+print(big)
+
+tiny=min('hello world')
+print(tiny)
+
+print('-----------------')
+
+print(float(99)/100)
+i=42
+print(type(i))
+f=float(i)
+print(f)
+print(type(f))
+print(1+2*float(3)/4-5)
+
+print('-----------------')
+
+
+x=5
+print('hello')
+def p_l():
+    print("asd")
+    print('zxc')
+
+print('yo')
+p_l()
+x=x+2
+print(x)
+
+print('-----------------')
+
+def greet(lang):
+    if lang == 'es':
+        print('hola')
+    elif lang == 'fr':
+        print('bonjour')
+    else:
+        print('hello')
+
+greet('en')
+greet('es')
+greet('fr')
+
+print('-----------------')
+
+def greet():
+    return "Hello"
+print(greet(), "asd")
+print(greet(), "zxc")
+
+print('-----------------')
+
+def addtwo(a, b):
+    added = a+b
+    return added
+x= addtwo(3, 4)
+print(x)
+
+print('-----------------')
+
+

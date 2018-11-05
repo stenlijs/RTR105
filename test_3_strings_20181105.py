7c9dbaeaaaaa1adcadbb39ecaf55fcdf0f33bb51
user <danielsstenlijs@gmail.com>
2018-11-05 11:33:42 +0200

    test_3_strings_20181105.py

---
+++ test_3_strings_20181105.py
@@ -0,0 +1,9 @@
+    1  VirtualBox --startvm XP
+    2  quartus
+    3  firefox &
+    4  rm -rf RTR105
+    5  git clone https://github.com/stenlijs/RTR105
+    6  cd RTR105/
+    7  ls -l
+    8  ./git-upload RTR105 test_3_strings_20181105.py 
+    9  history > test_3_strings_20181105.py 
